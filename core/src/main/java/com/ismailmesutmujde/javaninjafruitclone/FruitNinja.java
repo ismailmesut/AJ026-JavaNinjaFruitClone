c9359cf2a9dc05790cff1407a48a15203a579245
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-02 10:08:18 +0300

    updated: font generator

--- core/src/main/java/com/ismailmesutmujde/javaninjafruitclone/FruitNinja.java
+++ core/src/main/java/com/ismailmesutmujde/javaninjafruitclone/FruitNinja.java
@@ -1,7 +1,103 @@
 package com.ismailmesutmujde.javaninjafruitclone;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class FruitNinja extends ApplicationAdapter {
+public class FruitNinja extends ApplicationAdapter implements InputProcessor {
+
+    SpriteBatch batch;
+    Texture background;
+    Texture apple;
+    Texture bill;
+    Texture cherry;
+    Texture ruby;
+    BitmapFont font;
+    FreeTypeFontGenerator fontGen;
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        background = new Texture("ninjabackground.png");
+        apple = new Texture("apple.png");
+        cherry = new Texture("cherry.png");
+        ruby = new Texture("ruby.png");
+        bill = new Texture("bill.png");
+
+
+        Gdx.input.setInputProcessor(this);
+
+        fontGen = new FreeTypeFontGenerator(Gdx.files.internal("robotobold.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter params = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        params.color = Color.WHITE;
+        params.size = 40;
+        params.characters = "0123456789 ScreCutoplay:.+-";
+        font = fontGen.generateFont(params);
+    }
+
+    @Override
+    public void render () {
+        batch.begin();
+        batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        font.draw(batch,"Score: 0",30,40);
+        font.draw(batch,"Cut to play", Gdx.graphics.getWidth()*0.5f, Gdx.graphics.getHeight()*0.5f);
+        batch.end();
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        font.dispose();
+        fontGen.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
 }
