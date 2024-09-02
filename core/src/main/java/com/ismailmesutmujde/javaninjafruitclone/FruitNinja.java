4527482fa2a6d5b10a65d93d007892c54b13d551
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-02 10:37:20 +0300

    updated: parameters

--- core/src/main/java/com/ismailmesutmujde/javaninjafruitclone/FruitNinja.java
+++ core/src/main/java/com/ismailmesutmujde/javaninjafruitclone/FruitNinja.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.utils.TimeUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class FruitNinja extends ApplicationAdapter implements InputProcessor {
@@ -20,6 +21,14 @@ public class FruitNinja extends ApplicationAdapter implements InputProcessor {
     Texture ruby;
     BitmapFont font;
     FreeTypeFontGenerator fontGen;
+
+    int lives = 4;
+    int score = 0;
+
+    private double currentTime;
+    private double gameoverTime = -1.0f;
+
+
     @Override
     public void create () {
         batch = new SpriteBatch();
@@ -44,6 +53,28 @@ public class FruitNinja extends ApplicationAdapter implements InputProcessor {
     public void render () {
         batch.begin();
         batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+        double newTime = TimeUtils.millis()/1000.0;
+        System.out.println("newTime : " + newTime);
+
+        double frameTime = Math.min(newTime -currentTime, 0.3);
+        System.out.println("frameTime : " + frameTime);
+
+        float deltaTime = (float) frameTime;
+        System.out.println("deltaTime : " + deltaTime);
+
+        currentTime = newTime;
+
+        if (lives <= 0 && gameoverTime ==0f) {
+            // game over
+            gameoverTime = currentTime;
+        }
+        if (lives > 0) {
+            // game mode
+            for (int i = 0; i < lives; i++) {
+                batch.draw(apple,i*30f+20f, Gdx.graphics.getHeight()-25f,25f,25f);
+            }
+        }
         font.draw(batch,"Score: 0",30,40);
         font.draw(batch,"Cut to play", Gdx.graphics.getWidth()*0.5f, Gdx.graphics.getHeight()*0.5f);
         batch.end();
