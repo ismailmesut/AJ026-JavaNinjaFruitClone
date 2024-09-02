1aa835075b7d05b77a85e8485aa1dd0c191335cb
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-02 09:37:52 +0300

    initial commit

---
+++ core/src/main/java/com/ismailmesutmujde/javaninjafruitclone/FruitNinja.java
@@ -0,0 +1,7 @@
+package com.ismailmesutmujde.javaninjafruitclone;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class FruitNinja extends ApplicationAdapter {
+}
